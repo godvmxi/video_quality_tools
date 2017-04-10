c6343b943235af16a5455ff35c136b915e5fa4cd
bright.jiang <bright.jiang@infotm.com>
2017-04-10 22:00:13 +0800

    save

--- test.sh
+++ test.sh
@@ -3,33 +3,42 @@ H2V4=/nfs/bin/testbench_hevc_v8
 H2V1=/nfs/bin/testbench_hevc_v2
 H1V6=/nfse/bin/testbench_h
 IN_YUV=/nfs/yuv/nv12_1920p.yuv
-MAX_FRAME=45
+MAX_FRAME=40
+encode_h2v4(){
+	bps=$1
+	kbps=`expr $bps / 1000`
+	out_file="/nfs/h2v4/bps${kbps}k.hevc"
+	echo "#####################encode -> $bps : $kbps k  -->$out_file"
+	
+	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -a 1 -b $MAX_FRAME -L 180 --intraQpDelta 0 --bitPerSecond  $bps  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o $out_file
+}
 test_h2v4(){
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond    300000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps300k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond    500000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps500k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond    800000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps800k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond    800000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 30 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps800k_2.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   1000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps1000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   1500000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps1500k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   2000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps2000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   2500000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps2500k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   3000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps3000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   4000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps4000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   6000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps6000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond   8000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps8000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 100000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps10000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 100000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps12000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 140000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps14000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 160000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps16000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 180000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps18000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 200000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps20000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 220000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps22000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 240000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps24000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 280000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps28000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 320000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps32000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 360000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps36000k.hevc
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -b $MAX_FRAME -L 123 --bitPerSecond 400000000  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o /nfs/h2v4/bps40000k.hevc
+	encode_h2v4    8000000
+	encode_h2v4   10000000
+	encode_h2v4   12000000
+	encode_h2v4   14000000
+	encode_h2v4   16000000
+	encode_h2v4   18000000
+	encode_h2v4   20000000
+	encode_h2v4   22000000
+	encode_h2v4   24000000
+	encode_h2v4   28000000
+	encode_h2v4   32000000
+	encode_h2v4   36000000
+	encode_h2v4   40000000
 
+}
+test2(){
+	encode_h2v4     300000
+	encode_h2v4     500000
+	encode_h2v4     800000
+	encode_h2v4    1000000
+	encode_h2v4    1500000
+	encode_h2v4    2000000
+	encode_h2v4    3000000
+	encode_h2v4    4000000
+	encode_h2v4    6000000
+	encode_h2v4    8000000
 }
 test_h1v6(){
 	echo "h1v6"
