299853783eeb619069f5376a7f8aa683c6addea1
bright.jiang <bright.jiang@infotm.com>
2017-04-11 17:02:47 +0800

    save

--- test.sh
+++ test.sh
@@ -1,20 +1,36 @@
 #!/bin/sh 
-H2V4=/nfs/bin/testbench_hevc_v8
-H2V1=/nfs/bin/testbench_hevc_v2
-H1V6=/nfse/bin/testbench_h
+H2V8=/nfs/bin/testbench_hevc_v8
+H2V1=/nfs/bin/testbench_hevc_v1
+H1V6=/nfs/bin/testbench_h1v6_264
 IN_YUV=/nfs/yuv/nv12_1920p.yuv
-MAX_FRAME=40
+MAX_FRAME=20
 encode_h2v4(){
 	bps=$1
 	kbps=`expr $bps / 1000`
 	out_file="/nfs/h2v4/bps${kbps}k.hevc"
 	echo "#####################encode -> $bps : $kbps k  -->$out_file"
-	
-	/nfs/bin/testbench_hevc_v8 -i $IN_YUV -a 1 -b $MAX_FRAME -L 180 --intraQpDelta 0 --bitPerSecond  $bps  --tolMovingBitRate 1  -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o $out_file
+	$H2V8 -i $IN_YUV -a 1 -b $MAX_FRAME -L 180 --intraQpDelta 0 --bitPerSecond  $bps  --tolMovingBitRate 2 --picSkip 0 -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o $out_file
+}
+encode_h2v1(){
+	bps=$1
+	kbps=`expr $bps / 1000`
+	out_file="/nfs/h2v1/bps${kbps}k.hevc"
+	echo "#####################encode -> $bps : $kbps k  -->$out_file"
+	$H2V1 -i $IN_YUV -a 1 -b $MAX_FRAME -L 180 --intraQpDelta 0 --bitPerSecond  $bps  --tolMovingBitRate 2 --picSkip 0 -U 1 -u 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 --gopSize 1 --monitorFrames 15 -o $out_file
+
+
+}
+encode_h1v6(){
+	bps=$1
+	kbps=`expr $bps / 1000`
+	out_file="/nfs/h1v6/bps${kbps}k.h264"
+	echo "#####################encode h1v6 -> $bps : $kbps k  -->$out_file"
+	$H1V6 -i $IN_YUV -a 1 -b $MAX_FRAME -L 40 --intraQpDelta 0 --bitPerSecond  $bps   --picSkip 0 --picRc 1 --mbRc 1  -w 1920 -h 1088 -x 1920 -y 1088 -l 1   --intraPicRate 15 -f 15 -j 15 -g 15  -C 1 -o $out_file
+
+
 }
 test_h2v4(){
-	encode_h2v4    8000000
-	encode_h2v4   10000000
+	
 	encode_h2v4   12000000
 	encode_h2v4   14000000
 	encode_h2v4   16000000
@@ -28,6 +44,24 @@ test_h2v4(){
 	encode_h2v4   40000000
 
 }
+
+test_h1v6(){
+	echo "h1v6"
+	
+	encode_h1v6   28000000
+	encode_h1v6   32000000
+	encode_h1v6   36000000
+	encode_h1v6   40000000
+}
+test_h2v1(){
+	echo "h2v1"
+	encode_h2v1     300000
+	encode_h2v1     500000
+	encode_h2v1     800000
+	encode_h2v1    1000000
+
+}
+
 test2(){
 	encode_h2v4     300000
 	encode_h2v4     500000
@@ -39,12 +73,25 @@ test2(){
 	encode_h2v4    4000000
 	encode_h2v4    6000000
 	encode_h2v4    8000000
+	encode_h2v4   10000000
+	/////
+	encode_h1v6     300000
+	encode_h1v6     500000
+	encode_h1v6     800000
+	encode_h1v6    1000000
+	encode_h1v6    1500000
+	encode_h1v6    2000000
+	encode_h1v6    3000000
+	encode_h1v6    4000000
+	encode_h1v6    6000000
+	encode_h1v6    8000000
+	encode_h1v6   10000000
+	encode_h1v6   12000000
+	encode_h1v6   14000000
+	encode_h1v6   16000000
+	encode_h1v6   18000000
+	encode_h1v6   20000000
+	encode_h1v6   22000000
+	encode_h1v6   24000000
 }
-test_h1v6(){
-	echo "h1v6"
-}
-test_h2v1(){
-	echo "h2v1"
-}
-
-test_h2v4
+test_$1
